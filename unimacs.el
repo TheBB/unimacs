953d6ef9b3d2359b5f549748f8bb86496e7e19d2
Eivind Fonn <evfonn@gmail.com>
2014-08-25 23:00:38 +0200

    Some refactoring

--- unimacs.el
+++ unimacs.el
@@ -1,35 +1,69 @@
 (require 'grizzl)
 
 
-(defvar *unimacs/read-max-results* 10
+;; Global variables
+;; =================================================================================
+
+(defcustom unimacs/read-max-results 10
   "The maximum number of results to show.")
 
 (defvar *unimacs/result* nil
   "The search result.")
 
 (defvar *unimacs/hashmap* (make-hash-table)
-  "The hashmap containing auxiliary data.")
+  "A map mapping search entries to auxiliary data.")
 
 (defvar *unimacs/index* nil
-  "The search index.")
+  "The grizzl search index.")
 
 (defvar *unimacs/ncolumns* nil
-  "The number of columns.")
+  "The number of columns in the search (including search entry).")
 
 (defvar *unimacs/widths* nil
-  "The widths for each column.")
+  "The widths for each column. A list of length *unimacs/ncolumns*.")
 
 (defvar *unimacs/data* nil
-  "The data to search in.")
+  "The data to search in. A list of lists of strings.")
 
 (defvar *unimacs/selection* 0
   "The selected offset.")
+
+(defcustom unimacs/views
+  '((buffers . ((unimacs/src-buffers))))
+  "Available views.")
+
+
+
+;; Minor mode used when searching
+;; =================================================================================
+
+(defcustom unimacs/keymap (make-sparse-keymap)
+  "Internal keymap used by the minor-mode in `unimacs/completing-read'.")
+
+(define-key unimacs/keymap (kbd "<up>")   'unimacs/set-selection+1)
+(define-key unimacs/keymap (kbd "M-k")    'unimacs/set-selection+1)
+(define-key unimacs/keymap (kbd "<down>") 'unimacs/set-selection-1)
+(define-key unimacs/keymap (kbd "M-j")    'unimacs/set-selection-1)
+
+
+(define-minor-mode unimacs/mode
+  "Toggle the internal mode used by `unimacs/completing-read'."
+  nil
+  " Unimacs"
+  unimacs/keymap)
+
+
+
+;; Search functionality
+;; =================================================================================
+;; These functions are heavily inspired by the grizzl-read module by Chris Corbyn.
+;; They have been modified to display auxiliary data.
 
 
 (defun unimacs/selected-result (index)
   (elt (grizzl-result-strings *unimacs/result* index
                               :start 0
-                              :end   *unimacs/read-max-results*)
+                              :end   unimacs/read-max-results)
        (unimacs/current-selection)))
 
 
@@ -47,7 +81,7 @@
 
 (defun unimacs/current-selection ()
   (let ((max-selection
-         (min (1- *unimacs/read-max-results*)
+         (min (1- unimacs/read-max-results)
               (1- (grizzl-result-count *unimacs/result*)))))
     (max 0 (min max-selection *unimacs/selection*))))
 
@@ -80,7 +114,7 @@
 (defun unimacs/display-result (index prompt)
   (let* ((matches (grizzl-result-strings *unimacs/result* index
                                          :start 0
-                                         :end   *unimacs/read-max-results*)))
+                                         :end   unimacs/read-max-results)))
     (delete-all-overlays)
     (overlay-put (make-overlay (point-min) (point-min))
                  'before-string
@@ -92,7 +126,7 @@
     (set-window-text-height nil (max 3 (+ 2 (length matches))))))
 
 
-(defun unimacs/completing-read (prompt index)
+(defun unimacs/completing-read (prompt title index)
   (minibuffer-with-setup-hook
       (lambda ()
         (setq *unimacs/result* nil)
@@ -104,13 +138,13 @@
                               (grizzl-search (minibuffer-contents)
                                              index
                                              *unimacs/result*))
-                        (unimacs/display-result index prompt)))
+                        (unimacs/display-result index title)))
              (exitfun (lambda ()
                         (unimacs/mode -1)
                         (remove-hook 'post-command-hook    hookfun t))))
           (add-hook 'minibuffer-exit-hook exitfun nil t)
           (add-hook 'post-command-hook    hookfun nil t)))
-    (read-from-minibuffer ">>> ")
+    (read-from-minibuffer (concat prompt " "))
     (unimacs/selected-result index)))
 
 
@@ -130,22 +164,6 @@
   (unimacs/move-selection -1))
 
 
-(defvar *unimacs/keymap* (make-sparse-keymap)
-  "Internal keymap used by the minor-mode in `unimacs/completing-read'.")
-
-(define-key *unimacs/keymap* (kbd "<up>")   'unimacs/set-selection+1)
-(define-key *unimacs/keymap* (kbd "M-k")    'unimacs/set-selection+1)
-(define-key *unimacs/keymap* (kbd "<down>") 'unimacs/set-selection-1)
-(define-key *unimacs/keymap* (kbd "M-j")    'unimacs/set-selection-1)
-
-
-(define-minor-mode unimacs/mode
-  "Toggle the internal mode used by `unimacs/completing-read'."
-  nil
-  " Unimacs"
-  *unimacs/keymap*)
-
-
 (defun unimacs/max-lengths (lengths item)
   (if item
       (cons (max (car lengths) (length (car item)))
@@ -153,7 +171,7 @@
     lengths))
 
 
-(defun unimacs/do ()
+(defun unimacs/execute (title)
   (setq *unimacs/index* (grizzl-make-index (mapcar 'car *unimacs/data*)))
   (setq *unimacs/ncolumns* (apply 'max (mapcar 'length *unimacs/data*)))
   (setq *unimacs/widths*
@@ -163,21 +181,42 @@
   (clrhash *unimacs/hashmap*)
   (dolist (elt *unimacs/data*)
     (puthash (car elt) (cdr elt) *unimacs/hashmap*))
-  (unimacs/completing-read " Test" *unimacs/index*))
+  (unimacs/completing-read ">>>" (concat " Unimacs: " title) *unimacs/index*))
 
 
-(defun unimacs/buffers ()
-  (interactive)
+(defun unimacs/reset ()
+  (setq *unimacs/data* nil))
+
+
+(defun unimacs/view (view title callback)
+  (unimacs/reset)
+  (let* ((view-data (assq view unimacs/views))
+         (sources (cdr view-data)))
+    (dolist (elt sources)
+      (apply (car elt) (cdr elt)))
+    (funcall callback (unimacs/execute title))))
+
+
+
+;; Sources
+;; =================================================================================
+
+
+(defun unimacs/src-buffers ()
   (let* ((pre-buffers (mapcar 'buffer-name (buffer-list)))
          (filt-buffers (delq nil (mapcar (lambda (s)
                                            (if (eq 32 (string-to-char s)) nil s))
                                          pre-buffers))))
-    (setq *unimacs/data* (mapcar (lambda (bufname)
-                                   (list bufname
-                                         (with-current-buffer bufname mode-name)
-                                         (buffer-file-name (get-buffer bufname))))
-                                 filt-buffers))
-    (switch-to-buffer (unimacs/do))))
+    (dolist (bufname filt-buffers)
+      (setq *unimacs/data*
+            (cons (list bufname
+                        (with-current-buffer bufname mode-name)
+                        (buffer-file-name (get-buffer bufname)))
+                  *unimacs/data*)))))
 
+
+
+;; Fin
+;; =================================================================================
 
 (provide 'unimacs)
